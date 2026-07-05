7cde86d4173f6c33fd12ebf9b386065886b3a64a
Michael Telakowicz <teanmi6@gmail.com>
2026-07-05 01:45:47 -0400

    Fix config files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+import { env } from "process";
 
 const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
+  },
+  output: env.NODE_ENV === "production" ? "export" : undefined,
+  images: {
+    unoptimized: true,
   },
   allowedDevOrigins: ["https://housetab.com", "https://www.housetab.com", "http://localhost:3000"],
 };
